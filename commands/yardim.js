2a335d8b269de1a09cdee6b63b4ac00bc7b98c1f
UlusXii <44119720+UlusXii@users.noreply.github.com>
2021-03-14 04:55:08 +0300

    null

--- commands/yardim.js
+++ commands/yardim.js
@@ -1,5 +1,19 @@
+const Discord = require('discord.js')
 exports.run = async (client, message, args) =>{
-
+  let user = message.author
+  let sorgu = args[0]
+  let embed = new Discord.RichEmbed()
+  
+  .setTitle(`${user.username} sanırım yardıma ihtiyacın var.`,)
+  .addField(`Meslek komutlarımız: `, `__.meslek, .balık, .taksi, .odun, .maden__`)
+  .addField(`Eğlence komutlarımız: `,`__.blackjack, .slot, .soygun, .yazitura__`)
+  .addField(`Kişisel komutlar:`,`__.profil__`)
+  .setTimestamp()
+  .setImage("https://i.pinimg.com/originals/bb/df/a3/bbdfa3edc21e1de194952a5c3ac6bd80.gif")
+  .setColor("#D00BA6")
+  if(!sorgu){
+    message.channel.sendEmbed(embed)
+  }
 }
 exports.conf = {
     enabled: true,
